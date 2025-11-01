da61a6e4318ce794597f8edf0941d49336226246
plt1-coder <p.andreoficial@gmail.com>
2025-11-01 00:05:37 +0000

    desafio xadrez

---
+++ desafioxadrez.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main(){
+int r = 0;
+// TORRE 5 CASAS PARA A DIREITA
+printf("TORRE:\n");
+for(int i = 0; i <= 5; i++);
+{
+    printf("Direita\n"); // Direçao
+}
+
+
+// BISPO 5 CASAS DIAGONAL
+printf("BISPO:\n");
+for(int b = 0; b <= 5; b++);
+{
+    printf("Cima Direita\n"); // Direçao
+}
+
+// RAINHA 8 CASAS
+printf("RAINHA:\n");
+while (r <= 8)
+{
+    printf("Cima\n", r);
+    r++;
+}
+
+return 0;
+}
